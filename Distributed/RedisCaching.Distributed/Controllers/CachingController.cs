7b7c7533b42c16cf4684778e0e2da0d8e76a2258
BatuhanB <balibatuhan19@gmail.com>
2023-07-08 18:17:48 +0300

    added redis distributed caching example

---
+++ Distributed/RedisCaching.Distributed/Controllers/CachingController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace RedisCaching.Distributed.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CachingController : ControllerBase
+    {
+        private readonly IDistributedCache _distributedCache;
+
+        public CachingController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        [HttpGet("{key}/{value}")]
+        public async Task<IActionResult> Set(string key,string value)
+        {
+            await _distributedCache.SetStringAsync(key,value,options:new()
+            {
+                AbsoluteExpiration = DateTimeOffset.UtcNow.AddSeconds(15),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+            return Ok();
+        }
+
+
+        [HttpGet("{key}")]
+        public async Task<string> Get(string key)
+        {
+            return await _distributedCache.GetStringAsync(key);
+        }
+    }
+}
--- Distributed/RedisCaching.Distributed/Program.cs
+++ Distributed/RedisCaching.Distributed/Program.cs
@@ -1,3 +1,4 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +7,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddStackExchangeRedisCache(opt =>
+{
+    opt.Configuration= "localhost:1200";
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
