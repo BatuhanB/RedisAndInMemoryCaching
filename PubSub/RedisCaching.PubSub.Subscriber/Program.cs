b51c10419107b833e6713982a2d4603d8f465a7a
BatuhanB <balibatuhan19@gmail.com>
2023-07-09 13:11:56 +0300

    added redis pub/sub feature

---
+++ PubSub/RedisCaching.PubSub.Subscriber/Program.cs
@@ -0,0 +1,15 @@
+﻿using StackExchange.Redis;
+
+// creating a connection to the our redis database using StackExchange.Redis library
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1200");
+
+// Creating a subscriber using connection that we created above (subscriber => consumer, publish => publisher)
+var subscriber = connection.GetSubscriber();
+
+// Subscribing the channel that we specified in our publisher 
+await subscriber.SubscribeAsync("mychannel.*", (channel, message) =>
+{
+    Console.WriteLine($"Message: {message}");
+});
+
+Console.Read();
